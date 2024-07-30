211c1e02654ee802730eb7893888e515b27fe4fc
arnav-goel10 <arnav.goel1@outlook.com>
2024-07-30 23:30:06 +0530

    added homepage

---
+++ src/app/home/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+
+const page = () => {
+  return (
+    <>
+      <div className="home-container">
+        <div className="content-wrapper">
+          <div className="team-title">MARS ROVER TEAM</div>
+          10+ year old student-led team incubated at IIT Bombay which aims to
+          build rovers that are capable of performing extra-terrestrial
+          robotics, capable of autonomous traversal and, perform
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import React from "react";
+import logo from "../../images/logo.png";
+import "./navbar.css";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Link from "next/link";
+import Image from "next/image";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar
+        expand="lg"
+        fixed="top"
+        className="gradient-transparent custom-navbar"
+        variant="dark"
+      >
+        <Container fluid>
+          <Navbar.Brand as={Link} href="home" className="custom-navbar-brand">
+            <Image
+              src={logo}
+              alt="Logo"
+              style={{ width: "55px", height: "auto" }}
+            />
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="navbarSupportedContent" />
+
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="custom-nav">
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  href="home"
+                  className="custom-nav-link"
+                  aria-current="page"
+                  active
+                >
+                  Home
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  href="team"
+                  className="custom-nav-link"
+                  aria-current="page"
+                  active
+                >
+                  Team
+                </Nav.Link>
+              </Nav.Item>
+              <NavDropdown
+                title="Subsystems"
+                id="basic-nav-dropdown"
+                className="custom-nav-link"
+                data-bs-theme="dark"
+                active
+              >
+                <NavDropdown.Item as={Link} href="subsystems/mechanical">
+                  Mechanical & Manufacturing
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} href="subsystems/electronics">
+                  Electronics & Control
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} href="subsystems/software">
+                  Software
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} href="subsystems/science">
+                  Science
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} href="subsystems/business">
+                  Business & Media
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  href="sponsorships"
+                  className="custom-nav-link"
+                  aria-current="page"
+                  active
+                >
+                  Sponsorships
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  href="contact"
+                  className="custom-nav-link"
+                  aria-current="page"
+                  active
+                >
+                  Contact Us
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;

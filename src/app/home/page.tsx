5ff2fb9c49be839c6fe6cc46e2004a21d42b09b9
arnav-goel10 <arnav.goel1@outlook.com>
2024-08-04 04:19:36 +0530

    fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,12 @@
 "use client"; // Add this line at the top of the file
 import React from "react";
 import "./home.css";
+import mars from "../../images/mars.png";
 
 const page = () => {
   return (
     <>
-      <div className="home-container">
+      <div className="home-container mars-img">
         <div className="content-wrapper">
           <div className="team-title">Team Endeavour</div>
           <div className="about-us">
@@ -19,14 +20,7 @@ const page = () => {
       </div>
       <div className="home-container">
         <div className="content-wrapper">
-          <div className="team-title">Team Endeavour</div>
-          <div className="about-us">
-            We bring together a growing team of dedicated individuals from the
-            diverse fields of Engineering, Science, Design and Business to
-            design the next generation of Martian rovers and represent the
-            National University of Singapore at international robotics
-            competitions.
-          </div>
+          <div className="about-us"></div>
         </div>
       </div>
     </>

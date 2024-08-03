98f0e822cda82872d417323e677b0dc1ae3f4944
arnav-goel10 <arnav.goel1@outlook.com>
2024-08-04 00:01:50 +0530

    added footer and icons

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { FaLinkedin, FaInstagram } from "react-icons/fa";
+import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-heading">Follow us</div>
+      <div className="footer-columns">
+        <div className="footer-column">
+          <a
+            href="https://www.linkedin.com/company/nus-endeavour/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href="https://www.instagram.com/nusroverteam/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={24} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;

64433a2afca77ae405158c13f89d4ef91487f0ad
arnav-goel10 <arnav.goel1@outlook.com>
2024-08-09 00:49:43 +0530

    aa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/footer/Footer";
-import SmoothScroll from "@/components/smoothscroll/Scroll";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +19,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SmoothScroll />
         <NavBar />
         {children}
         <Footer />

c7c55513b234a8d1b361dcf0cdc7429a8347de26
arnav-goel10 <arnav.goel1@outlook.com>
2024-08-09 00:48:46 +0530

    aa

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 import "./home.css";
 
-const page = () => {
+const Page = () => {
   return (
-    <>
-      <div className="home-container mars">
-        <div className="mars-wrapper">
+    <div id="scroll">
+      <section className="mars-section">
+        <div className="content-wrapper">
           <div className="about-us">
             We bring together a growing team of dedicated individuals from the
-            diverse fields of Engineering, Science, Design and Business to
+            diverse fields of Engineering, Science, Design, and Business to
             design the next generation of Martian rovers and represent the
             National University of Singapore at international robotics
             competitions.
           </div>
         </div>
-      </div>
-      <div className="home-container">
-        <div className="vision-wrapper"></div>
-      </div>
-    </>
+      </section>
+      <section className="vision-section">
+        <div className="content-wrapper">
+          <div className="vision-content">test</div>
+        </div>
+      </section>
+    </div>
   );
 };
 
-export default page;
+export default Page;

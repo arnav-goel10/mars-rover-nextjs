7b6c7cf577e503d1d8a614d39d0d2a58de4a9bf9
Arnav Goel <arnav.goel1@outlook.com>
2025-01-04 01:31:20 +0530

    main homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./home.css";
 import Footer from "@/components/footer/Footer";
-import vision from "../../images/vision.png";
-import Image from "next/image";
 
 const Page = () => {
   const [showFooter, setShowFooter] = useState(false);
@@ -46,60 +44,43 @@ const Page = () => {
     <div id="scroll">
       <section className="mars-section">
         <div className="content-wrapper">
-          <div className="about-us">
-            We bring together a growing team of dedicated individuals from the
-            diverse fields of Engineering, Science, Design, and Business to
-            design the next generation of Martian rovers and represent the
-            National University of Singapore at international robotics
-            competitions.
+          <h1 className="mars-heading">
+            Designing the <br />
+            Future of Martian Rovers
+          </h1>
+          <p className="mars-subheading">
+            Bringing Innovative Solutions for Mars Exploration
+          </p>
+          <div className="button-group">
+            <a href="/contact" className="button button-dark">
+              Join Us
+            </a>
+            <a href="/subsystems" className="button button-outline">
+              Explore Our Subsystems
+            </a>
           </div>
         </div>
       </section>
-      <section className="vision-section">
+
+      <section>
         <div className="content-wrapper">
-          <div className="vision-content">
-            <div className="text-container">
-              <h2>Mission</h2>
-              <p>
-                To be the first Singaporean & South East Asian team to qualify &
-                win the University Rover Challenge 2025.
-              </p>
-            </div>
-            <div ref={visionRef1} className="image-container">
-              <Image
-                src={vision}
-                alt="Your Image Description"
-                width={300}
-                height={250}
-              />
-            </div>
-          </div>
-          <div className="vision-content">
-            <div ref={visionRef2} className="image-container">
-              <Image
-                src={vision}
-                alt="Your Second Image Description"
-                width={300}
-                height={250}
-              />
-            </div>
-            <div className="text-container">
-              <h2>Vision</h2>
-              <p style={{ fontSize: "1.1rem" }}>
-                Akin to the pioneering rovers on Mars, our team&apos;s spirit
-                embodies the values of Curiosity, Perseverance, and Ingenuity.
-                We strive to explore, innovate, and overcome adversities just as
-                our robotic counterparts do on the Red Planet.
-              </p>
-            </div>
-          </div>
+          <h2>Mission</h2>
+          <p>
+            To be the first Singaporean & South East Asian team to qualify & win
+            the University Rover Challenge 2025.
+          </p>
+
+          <h2>Vision</h2>
+          <p style={{ fontSize: "1.1rem" }}>
+            Akin to the pioneering rovers on Mars, our team&apos;s spirit
+            embodies the values of Curiosity, Perseverance, and Ingenuity. We
+            strive to explore, innovate, and overcome adversities just as our
+            robotic counterparts do on the Red Planet.
+          </p>
         </div>
       </section>
-      <section className="vision-section">
-        <div className="content-wrapper">
-          <div className="vision-content">test</div>
-        </div>
-      </section>
+      <section> test lomao</section>
+      <section> test 2222</section>
 
       <Footer isVisible={showFooter} />
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/footer/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "NUS Mars Rover",
@@ -18,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <NavBar />
         {children}
       </body>

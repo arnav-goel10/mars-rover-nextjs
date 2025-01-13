ae71c9f23f09431896f9eaf6940c4eb83c59d84d
Arnav Goel <arnav.goel1@outlook.com>
2025-01-13 11:40:42 +0800

    aaa

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,23 +1,86 @@
-import React from "react";
-
-import { teamMembersList } from "@/data/teamMembersList";
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import NavBar from "@/components/navbar/NavBar";
 import "./team.css";
 import TeamCard from "@/components/teamcard/TeamCard";
-import NavBar from "@/components/navbar/NavBar";
+import { teamMembersList } from "@/data/teamMembersList";
+import Footer from "@/components/footer/Footer";
 
-const page = () => {
+const SCROLL_HIDE_THRESHOLD = 25;
+
+const Page = () => {
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [showFooter, setShowFooter] = useState(false);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      // Hide navbar when scrolling down
+      if (currentScrollY - lastScrollY.current > SCROLL_HIDE_THRESHOLD) {
+        setIsNavVisible(false);
+      }
+
+      // Show navbar when scrolling up
+      if (lastScrollY.current - currentScrollY > SCROLL_HIDE_THRESHOLD) {
+        setIsNavVisible(true);
+      }
+
+      // Check if scrolled to the bottom to show the footer
+      const scrolledToBottom =
+        window.innerHeight + window.scrollY >=
+        document.documentElement.scrollHeight;
+      setShowFooter(scrolledToBottom);
+
+      // Update last scroll position
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <NavBar />
-      <div className="team-heading" style={{ marginTop: 250 }}>
-        <TeamCard
-          image="/images/mars1.jpg"
-          name="Our Team"
-          designation="Meet the team"
-        />
+      <div className={`navbar-wrapper ${isNavVisible ? "visible" : "hidden"}`}>
+        <NavBar />
       </div>
+      <div className="team-section">
+        <div className="team-container">
+          <div className="team-heading">
+            <h1>Our Team.</h1>
+          </div>
+          <div className="team-description">
+            <p>
+              The NUS Mars Rover Team combines expertise and passion for space
+              exploration. Together, we are dedicated to advancing technology
+              and innovation in the field of robotics, aiming to contribute to
+              future missions on Mars. With a diverse set of skills and a shared
+              vision, we strive to push the boundaries of what is possible in
+              extraterrestrial research. Join us on this exciting journey as we
+              work towards making history in space exploration!
+            </p>
+          </div>
+        </div>
+        <div className="team-members-wrapper">
+          <div className="team-members">
+            {teamMembersList.map((member, index) => (
+              <TeamCard
+                key={index}
+                image={member.image}
+                name={member.name}
+                designation={member.designation}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer isVisible={showFooter} />
     </>
   );
 };
 
-export default page;
+export default Page;
--- src/data/teamMembersList.ts
+++ src/data/teamMembersList.ts
@@ -1,21 +1,74 @@
 import random from "../images/random.png";
+
 export const teamMembersList = [
   {
     image: random,
-    name: "Narendra Modi",
-    designation: "Prime Minister",
-    linkedin: "https://www.linkedin.com",
+    name: "Mr. Eugene Ee",
+    designation: "Project Supervisor",
   },
   {
     image: random,
-    name: "Donald Trump",
-    designation: "President",
-    linkedin: "https://www.linkedin.com",
+    name: "Mr. Nicholas Chew",
+    designation: "Project Supervisor",
   },
   {
     image: random,
-    name: "Joe Biden",
-    designation: "Retired",
-    linkedin: "https://www.linkedin.com",
+    name: "Arshita Dwivedi",
+    designation: "Team Lead",
+  },
+  {
+    image: random,
+    name: "Vaibhav Dileep Pillai",
+    designation: "Dy Team Lead, Software Lead",
+  },
+  {
+    image: random,
+    name: "Saksham Mathur",
+    designation: "Mechanical Lead",
+  },
+  {
+    image: random,
+    name: "Gopal R. Vejaya",
+    designation: "Electrical Lead",
+  },
+  {
+    image: random,
+    name: "Pan Yongjing",
+    designation: "GNSS & Comms Lead",
+  },
+  {
+    image: random,
+    name: "Shyam Krishna",
+    designation: "AutoNav Lead",
+  },
+  {
+    image: random,
+    name: "Jian Ling",
+    designation: "Science Lead",
+  },
+  {
+    image: random,
+    name: "Shruti Moovendran",
+    designation: "Business & Media Lead",
+  },
+  {
+    image: random,
+    name: "Parv Bhadra",
+    designation: "Chassis Lead",
+  },
+  {
+    image: random,
+    name: "Maia Goh",
+    designation: "Wheels Lead",
+  },
+  {
+    image: random,
+    name: "Tai Jia Xuan",
+    designation: "Chemistry Lead",
+  },
+  {
+    image: random,
+    name: "Jin",
+    designation: "Spectroscopy Lead",
   },
 ];

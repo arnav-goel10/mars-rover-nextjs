abeedb6bee8e397e51a880842a465c057fdb140d
Arnav Goel <arnav.goel1@outlook.com>
2025-01-25 18:34:40 +0800

    aa

---
+++ src/app/sponsorships/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "@/components/navbar/NavBar";
+
+const page = () => {
+    return (
+        <>
+            <NavBar/>
+            <div>page</div>
+        </>
+    )
+};
+
+export default page;
